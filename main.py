1c4a20328109b4774212800614f3683a85d115fa
Houtamelo <88971943+Houtamelo@users.noreply.github.com>
2025-10-20 11:04:17 -0300

    Fix invalid index exception when APP_FONT fails to load.
    If that happens, we fall back to the default font family. The interface may look ugly but at least it will work.

--- main.py
+++ main.py
@@ -9,16 +9,23 @@ from config.app import APP_FONT, APP_FONT_SIZE, APP_STYLE
 def main():
     app = QApplication(sys.argv)
     app.setStyleSheet(APP_STYLE)
-    app.setFont(_build_font())
+    _build_and_set_font(app)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
 
 
-def _build_font():
-    font = QFontDatabase.addApplicationFont(APP_FONT)
-    font_family = QFontDatabase.applicationFontFamilies(font)[0]
-    return QFont(font_family, APP_FONT_SIZE)
+def _build_and_set_font(app):
+    font_id = QFontDatabase.addApplicationFont(APP_FONT)
+    # Due to platform issues, the font may fail to load. In that case, 
+    #   we can just use the default font but with `APP_FONT_SIZE` instead of the default size.
+    if font_id != -1:
+        font_family = QFontDatabase.applicationFontFamilies(font)[0]
+    else:
+        font_family = app.font().family()
+    
+    font = QFont(font_family, APP_FONT_SIZE)
+    app.setFont(q_font)
 
 
 if __name__ == "__main__":
